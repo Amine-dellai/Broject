c668da321efb0dd5dae2464c8d0a991b65e83dda
amin <meddellai86@gmail.com>
2024-10-10 18:58:43 +0200

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/{name}', name: 'app_home')] // Assurez-vous que le paramètre de route {name} est défini
+    public function home(string $name): Response
+    {
+        return $this->render('index.html.twig', ['name' => $name]);
+    }
+}
